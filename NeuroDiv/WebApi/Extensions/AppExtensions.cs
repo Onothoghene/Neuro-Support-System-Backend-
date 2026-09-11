25a177144edf8a419bf860502263256c837c0dd7
Onothoghene <marioonosnorbert@gmail.com>
2026-09-12 00:51:54 +0100

    minor fix

--- NeuroDiv/WebApi/Extensions/AppExtensions.cs
+++ NeuroDiv/WebApi/Extensions/AppExtensions.cs
@@ -21,7 +21,7 @@ namespace WebApi.Extensions
                 {
                     options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
                 }
-                options.RoutePrefix = string.Empty;
+                //options.RoutePrefix = string.Empty;
             });
 
         }
